ae823db55af285644fc0e13fce5d172f2c5076b7
OMR <cartoon6676@gmail.com>
2026-04-18 23:32:16 +0200

    prevent rereders via celver structre

---
+++ src/components/Counter/ConfigureCounter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useState } from "react";
+import { log } from "../../log.js";
+function ConfigureCounter({ handleSetClick }) {
+  log("<ConfigureCounter /> rendered");
+  const [enteredNumber, setEnteredNumber] = useState(0);
+  function handleChange(event) {
+    setEnteredNumber(+event.target.value);
+  }
+
+  return (
+    <section id="configure-counter">
+      <h2>Set Counter</h2>
+      <input type="number" onChange={handleChange} value={enteredNumber} />
+      <button
+        onClick={() => {
+          handleSetClick(enteredNumber);
+          setEnteredNumber(0);
+        }}
+      >
+        Set
+      </button>
+    </section>
+  );
+}
+
+export default ConfigureCounter;
